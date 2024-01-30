1e8c54d354df8e34c75f574c9cf82673af97d86c
Supertiger <saawalkhn@googlemail.com>
2024-01-30 23:44:22 +0000

    fixed bugs

--- sw.js
+++ sw.js
@@ -12,14 +12,16 @@ self.addEventListener('fetch', function(event) {
   
   const pathname = new URL(url).pathname;
 
+  
   if (!url.startsWith(location.origin)) return;
-
+  
+  
   const basePath = "/new-supertiger-os"
-
+  // const basePath = ""
 
   if (!pathname.startsWith(basePath + "/hdd")) return;
   
-  event.respondWith( (async () => {
+  event.respondWith((async () => {
       const blob = await getFile(pathname.substring(4 + basePath.length))
       if (!blob) {
         return new Response("Not found", {status: 404})
