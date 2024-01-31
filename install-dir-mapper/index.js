27f51e1ec981de62935587eae4c4f687c0144163
Supertiger <saawalkhn@googlemail.com>
2024-01-31 15:37:53 +0000

    added solidjs

--- install-dir-mapper/index.js
+++ install-dir-mapper/index.js
@@ -4,10 +4,12 @@ const fs = require("fs/promises");
 
 const main = async () => {
   const paths = await glob("../install-files/**")
-  const transformedPaths = paths.map((p) => ({
-    path: path.relative("../install-files", p).replace(/\\/g, "/"),
-    name: path.basename(p)
-  }));
+  const transformedPaths = paths
+    .filter(p => !p.endsWith(".d.ts"))
+    .map((p) => ({
+      path: path.relative("../install-files", p).replace(/\\/g, "/"),
+      name: path.basename(p)
+    }));
   await fs.writeFile("../install-map.json", JSON.stringify(transformedPaths))
 }
 
--- install-files/os/apps/FileBrowser.js
+++ install-files/os/apps/FileBrowser.js
@@ -4,6 +4,10 @@ import { Application } from "../Application.js";
 import { Window } from "../Window.js";
 import { humanFileSize, styled } from "../utils.js";
 
+import {onCleanup, For, createSignal, Show, createEffect, onMount} from '../solid-js/index.js'
+import {render} from '../solid-js/web/index.js'
+
+import html from '../solid-js/html/index.js'
 
 function start() {
   const app = new Application();
@@ -29,6 +33,7 @@ function start() {
         border-radius: 6px;
         height: 38px;
         user-select: none;
+        overflow: hidden;
         .arrow {
           opacity: 0.6;
           font-size: 14px;
@@ -42,6 +47,14 @@ function start() {
           display: flex;
           align-items: center;
           justify-content: center;
+          animation: slidePartUp 0.2s;
+          animation-fill-mode: forwards;
+          &.lastPart {
+            cursor: initial;
+            &:hover {
+              background-color: initial;
+            }
+          }
           &:hover {
             background-color: rgba(255,255,255,0.2);
           }
@@ -59,11 +72,10 @@ function start() {
           padding: 10px;
           border-radius: 6px;
           transition: background-color 0.2s;
-          .icon {
-            &.folder {
-              color: #ffd165;
-            }
+          &.folder .icon {
+            color: #ffd165;
           }
+ 
           &:hover {
             background-color: rgba(255,255,255,0.2);
           }
@@ -74,33 +86,67 @@ function start() {
           }
         }
       }
+
+      @keyframes slidePartUp {
+        from {
+          transform: translateY(100%);
+        }
+        to {
+          transform: translateY(0);
+        }
+      }
+
     `);
 
-    let currentPath = "/";
-    let files = []
+    // let currentPath = "/";
+    // let files = []
 
-    const addressBar = createAddressBar();
+    // const addressBar = createAddressBar();
     
-    const filesList = createFilesList(win, async (path) => {
-      updatePath(currentPath + path + "/")
-    });
+    // const filesList = createFilesList(win, async (path) => {
+    //   updatePath(currentPath + path + "/")
+    // });
 
 
-    const updatePath = async (path) => {
-      currentPath = path;
-      addressBar.update(currentPath);
-      let files = await fs.readdir(currentPath);
-      filesList.update(files);
+    // const updatePath = async (path) => {
+    //   currentPath = path;
+    //   addressBar.update(currentPath);
+    //   files = await fs.readdir(currentPath);
+    //   filesList.update(files);
+    // }
+    // updatePath("/")
+    
+
+    // addressBar.onReplaceEventRequest(updatePath)
+    
+
+
+    // win.appContainer.appendChild(addressBar.element);
+    // win.appContainer.appendChild(filesList.element);
+
+
+    const App = () => {
+      const [parts, setParts] = createSignal(["os"]);
+      const [files, setFiles] = createSignal([]);
+
+      const fullPath = () => {
+        return "/" + parts().join("/");
+      }
+
+      createEffect(() => {
+        fs.readdir(fullPath()).then(files => setFiles(files))
+      })
+      
+
+      return html`
+        <div>
+          <${AddressBar} replaceParts=${newParts => setParts(newParts)} parts=${() => parts()} />
+          <${FileList} appendPath=${newPath => setParts([...parts(), newPath])} files=${() => files()} />
+        </div>
+      `;
     }
-    updatePath("/")
-    
 
-    addressBar.onReplaceEventRequest(updatePath)
-    
-
-
-    win.appContainer.appendChild(addressBar.element);
-    win.appContainer.appendChild(filesList.element);
+    const destroy = render(App, win.appContainer)
 
 
     app.openWindow(win);
@@ -109,56 +155,69 @@ function start() {
 }
 
 
-const createAddressBar = () => {
-  let replacePathEvent;
-  const element = document.createElement("div");
-  element.classList.add("addressBar");
+const AddressBar = (props) => {
 
-  element.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target instanceof HTMLDivElement) {
-      const partEl = target.closest(".part");
-      if (partEl) {
-        const path = partEl.getAttribute("data-full-path");
-        if (path) replacePathEvent?.(path);
-      }
+  const PartItem = (partProps) => {
+
+    const onClick = () => {
+      const pathFromHere = props.parts.slice(0, partProps.index + 1)
+      props.replaceParts(pathFromHere);
     }
-  })
 
-  const update = (newVal) => {
+    const isLastPart = () =>partProps.part === "~" && !props.parts.length || partProps.index === props.parts.length - 1
 
-    element.innerHTML = "";   
-    const parts = ("~/" + newVal).split("/").filter(part => part);
-
-    for (let i = 0; i < parts.length; i++) {
-      const part = parts[i];
-      const partEl = document.createElement("div");
-      partEl.classList.add("part");
-      partEl.innerText = part;
-
-      let pathFromHere = parts.slice(0, i + 1).join("/");
-
-      if (pathFromHere === "~") pathFromHere = "/";
-      else pathFromHere = pathFromHere.substring(1) + "/";
-
-      partEl.setAttribute("data-full-path", pathFromHere);
-
-      if (i !== 0) element.innerHTML += `<span class="material-icons-round arrow">chevron_right</span>`
-      element.appendChild(partEl);
-    }
-   
-  };
-
-  update("/")
-
-  return {
-    element,
-    update,
-    onReplaceEventRequest: (cb) => {
-      replacePathEvent = cb;
-    }
+    return html`
+      <${Show} when=${() =>!partProps.hideArrow}><span class="material-icons-round arrow">chevron_right</span><//>
+      <div onClick=${onClick} class=${() => `part ${isLastPart() ? "lastPart" : ""}`}>${partProps.part}</div>
+    `
   }
 
+  return html`
+    <div class="addressBar">
+      <${PartItem} part="~" hideArrow={true} />
+      <${For} each=${() => props.parts}>
+        ${(part, i) => html`<${PartItem} index=${() => i()} part=${() => part}/>`}
+      <//>
+    </div>
+  `
+}
+
+
+const FileList = (props) => {
+  
+  const FileItem = (fileItemProps) => {
+    const [metadata, setMetadata] = createSignal(null)
+
+    const basename = () => path.basename(fileItemProps.path)
+    const onDblClick = () => {
+      props.appendPath(basename())
+    }
+
+    onMount(() => {
+      fs.stat(fileItemProps.path).then((res) => {
+        setMetadata(res)
+      })
+    })
+
+    return html`
+      <div class=${() => `fileItem ${metadata()?.directory ? "folder" : "file"}`} ondblclick=${onDblClick}>
+        <span class="icon material-icons-round">${() => metadata()?.directory ? "folder" : "insert_drive_file"}</span>
+        ${() => basename()}
+        <div class='data'>
+            ${() => metadata()?.type?.split("/")[1] || "Folder"} ${() => metadata()?.directory ? "" : ` • ${humanFileSize(metadata()?.size)}`}
+        </div>
+      </div>
+    `
+  }
+
+  return html`
+    <div class="fileList">
+      <${For} each=${() => props.files}>
+        ${(path, i) => html`<${FileItem} path=${() => path} />`}
+      <//>
+    </div>
+  `
+  
 }
 
 
@@ -171,6 +230,7 @@ const createFilesList = (win, appendDir) => {
     const target = e.target;
     if (target instanceof HTMLDivElement) {
       const closestTo = target.closest(".fileItem");
+      if (!closestTo.classList.contains("folder")) return;
       const path = closestTo.getAttribute("data-path");
       appendDir(path);
       return;
@@ -197,7 +257,7 @@ const createFilesList = (win, appendDir) => {
       
       fs.stat(file).then(res => {
         const isFolder = res.directory;
-        icon.classList.add(isFolder ? "folder" : "file");
+        itemEl.classList.add(isFolder ? "folder" : "file");
 
         icon.innerText = isFolder ? "folder" : "insert_drive_file";
           itemEl.innerHTML += `
